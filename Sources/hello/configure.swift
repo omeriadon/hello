f41296a28319f2422250b4efae99d593c19a1369
omeriadon <adon.omeri@student.education.wa.edu.au>
2025-10-05 21:04:01 +0800

    made ssl valid, https

--- Sources/hello/configure.swift
+++ Sources/hello/configure.swift
@@ -1,3 +1,4 @@
+import NIOSSL
 import Vapor
 
 /// configures your application
@@ -7,6 +8,16 @@ public func configure(_ app: Application) async throws {
 
 	app.http.server.configuration.hostname = "0.0.0.0"
 
+	let certificate = try NIOSSLCertificate.fromPEMFile("/Users/adonomeri/vapor-certs/fullchain.pem")
+	let privateKey = try NIOSSLPrivateKey(file: "/Users/adonomeri/vapor-certs/privkey.pem", format: .pem)
+
+	app.http.server.configuration.tlsConfiguration = .makeServerConfiguration(
+		certificateChain: certificate.map { .certificate($0) },
+		privateKey: .privateKey(privateKey)
+	)
+
+	// Optional: set port 443 for HTTPS
+	app.http.server.configuration.port = 443
 
 	// register routes
 	try routes(app)
